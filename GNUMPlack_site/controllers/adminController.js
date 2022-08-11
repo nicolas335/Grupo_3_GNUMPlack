284b832daad18611116593e6b6f112831ce7e4af
nicolas335 <nicolasemanuel335@gmail.com>
2022-08-11 15:39:58 -0300

    agrego un poco de estilo a las vistas admin y cambio nombres para poder crear un edit.ejs de tipo formulario

--- GNUMPlack_site/controllers/adminController.js
+++ GNUMPlack_site/controllers/adminController.js
@@ -2,10 +2,10 @@ module.exports = {
     create :(req, res) =>{
         return res.render('admin/create');
     },
-    edit :(req, res) =>{
-        return res.render('admin/edit');
-    },
     list :(req, res) =>{
         return res.render('admin/list');
+    },
+    edit :(req, res) =>{
+        return res.render('admin/edit');
     },
 }
--- GNUMPlack_site/routes/admin.js
+++ GNUMPlack_site/routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 let {create, edit, list} = require('../controllers/adminController');
 
 router.get('/create', create);
-router.get('/edit', edit);
 router.get('/list', list);
+router.get('/edit', edit);
 
 module.exports = router;
