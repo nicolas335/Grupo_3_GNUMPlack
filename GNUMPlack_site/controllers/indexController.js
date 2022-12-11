836803179b4dde55988a0e294a7516be703ebaf0
nicolas335 <nicolasemanuel335@gmail.com>
2022-12-11 17:08:35 -0300

    arreglos en home

--- GNUMPlack_site/controllers/indexController.js
+++ GNUMPlack_site/controllers/indexController.js
@@ -2,10 +2,13 @@ const db = require('../database/models');
 const {Op} = require('Sequelize');
 module.exports = {
     home : (req, res) =>{
+      const categories = ["Placa de yeso","Placa de yeso laminado","Placa de yeso con fibras","Placa de lana de madera","Placa de cemento"];
       db.Products.findAll()
         .then(products => {
             /* return res.send(products) */
-            return res.render("home",{products})
+            const saleProducts = products.filter(p => p.conditions_id === 2);
+            const featuredProducts = products.filter(p => p.conditions_id === 1);
+            return res.render( "home", {products ,saleProducts ,featuredProducts ,categories })
         })
         .catch(err => res.send(err))
         /* return res.render("home") */
