ea0c0f3d95344f7faf31344469611aeae76aa166
nicolas335 <nicolasemanuel335@gmail.com>
2022-12-23 07:03:09 -0300

    subo contador de cart

--- GNUMPlack_site/public/js/cart.js
+++ GNUMPlack_site/public/js/cart.js
@@ -1,38 +1,33 @@
-/* window.addEventListener('load', function() {
+window.addEventListener('load', function() {
     const $ = (e) => document.querySelector(e);
     const $all = (e) => document.querySelectorAll(e);
 
     let valorActual = 1;
 
-    const botones = $all('.boton');
-    const contador = $('#p-cantidad');
+    let precioSumado = 0;
+
+    const botonesMas = $all('.botonMas');
+    const botonesMenos = $all('.botonMenos');
     const contadoresClass = $all('.p-contador');
+    const precios = $all('.p-price');
 
-
-    botones.forEach((boton, i) =>{
-
-        boton.addEventListener('click', e => {
-
-            const estilos = e.currentTarget.classList;
-
-            if(estilos.contains('mas')){
-                valorActual++;
-                
-            }else if(estilos.contains('menos')){
-                valorActual--;
-
-            }else{
-                valorActual = 1;
-            }
-
-            
-            valorActual < 1? valorActual = 1: contador.textContent = valorActual;
-            
-            
+    for (let i = 0; i < contadoresClass.length; i++) {
+        botonesMas[i].addEventListener('click', e => {
+            /* precioSumado = 0; */
+            valorActual++;
+            valorActual < 1? valorActual = 1:contadoresClass[i].textContent = valorActual;
+            /* precioSumado = precios[i].textContent * valorActual;
+            precios[i].textContent = precioSumado; */
         })
+        botonesMenos[i].addEventListener('click', e => {
+            /* precioSumado = 0; */
+            valorActual--;
+            valorActual < 1? valorActual = 1: contadoresClass[i].textContent = valorActual;
+            /* precioSumado = precios[i].textContent * valorActual;
+            precios[i].textContent = precioSumado; */
     })
-        
-  
-   
-}) */
+    }
+
+      
+})
 
