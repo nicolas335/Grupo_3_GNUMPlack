e3654afebb96e42f69744e4f497ebeb3ca4ce352
nicolas335 <nicolasemanuel335@gmail.com>
2022-12-23 08:28:01 -0300

    agrego boton comprar funcional y estilos al mismo

--- GNUMPlack_site/public/js/cart.js
+++ GNUMPlack_site/public/js/cart.js
@@ -4,13 +4,40 @@ window.addEventListener('load', function() {
 
     let valorActual = 1;
 
-    let precioSumado = 0;
+    /* let precioSumado = 0; */
 
     const botonesMas = $all('.botonMas');
     const botonesMenos = $all('.botonMenos');
     const contadoresClass = $all('.p-contador');
-    const precios = $all('.p-price');
+    /* const precios = $all('.p-price'); */
+    const comprar = $('.comprarOrden');
 
+    comprar.addEventListener('submit', (e) => {
+        e.preventDefault();
+                Swal.fire({
+                customClass: {
+                    confirmButton: 'swalBtnColor',
+                    cancelButton: 'swalBtnColor'
+                },
+
+                title: '¡Compra realizada con éxito!',
+                text: "Tu compra ha sido realizada exitosamente, por favor retirala en tu tienda GNUMPlack mas cercana: https://goo.gl/maps/QXuYudfsFKX7jdco6",
+                icon: 'success',
+                background: "#ebebeb",
+                showCancelButton: false,
+                confirmButtonText: 'Genial!',
+                showClass: {
+                    popup: 'animate__animated animate__fadeInDown'
+                },
+
+                }).then((result) => {
+
+                    if (result.isConfirmed) {
+                        comprar.submit();
+                    }
+
+                })
+    })
     for (let i = 0; i < contadoresClass.length; i++) {
         botonesMas[i].addEventListener('click', e => {
             /* precioSumado = 0; */
